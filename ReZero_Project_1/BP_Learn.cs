bac56b71bf6f1450e1ee5b347570963d28230d71
kimminkuk <rlaal567@gmail.com>
2020-03-11 23:49:57 +0900

    Update to BP_Start Object

--- ReZero_Project_1/BP_Learn.cs
+++ ReZero_Project_1/BP_Learn.cs
@@ -10,8 +10,29 @@ namespace ReZero_Project_1
     {
         public void BP_Start() 
         {
-            int Lable = 0;
-            int new_Lable = 0;
+            //int 
+            int Bias = 1;
+            double L_N_G = 0.2;
+
+            double[] Input      = new double[10];
+            double[] Sigmoid    = new double[100];
+            double[] Delta      = new double[100];
+            double[] Sum        = new double[100];
+
+            double[] Sum_Output     = new double[10];
+            double[] Sigmoid_Output = new double[10];
+            double[] Delta_Output   = new double[10];
+            double[] Error          = new double[10];
+            double[] Error_Result   = new double[10];
+            double[] Error_add      = new double[10];
+
+
+            double[] Bias_Weight   = new double[10];
+
+            double[] Weight_Input_Layer  = new double[10];
+            double[] Weight_Output_Layer = new double[10];
+            double[,] Weight_Layer = new double[10,10];
+
         }
 
     }
